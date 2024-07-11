8389d0a7d5e9af081ab39c4cc801027148cc7111
Victoria Levine <vkpanchenko@gmail.com>
2024-07-11 23:43:25 +0200

    Add project files.

---
+++ FinNetworkTask/FintechNetwork.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinNetworkTask
+{
+    internal class FintechNetwork
+    {
+        private Dictionary<int, List<int>> userInsights = new Dictionary<int, List<int>>();
+        private Dictionary<int, HashSet<int>> userConnections = new Dictionary<int, HashSet<int>>();
+
+        public void ShareInsight(int userId, int insightId)
+        {
+            if (!userInsights.ContainsKey(userId))
+            {
+                userInsights[userId] = new List<int>();
+            }
+            userInsights[userId].Insert(0, insightId);  // Prepending to simulate latest first
+        }
+
+        public List<int> GetLatestInsights(int userId)
+        {
+            var allInsights = new List<int>();
+
+            if (userInsights.ContainsKey(userId))
+            {
+                allInsights.AddRange(userInsights[userId]);
+            }
+
+            if (userConnections.ContainsKey(userId))
+            {
+                foreach (var friendId in userConnections[userId])
+                {
+                    if (userInsights.ContainsKey(friendId))
+                    {
+                        allInsights.AddRange(userInsights[friendId]);
+                    }
+                }
+            }
+
+            return allInsights.OrderByDescending(id => id).Take(10).ToList();
+        }
+
+        public void ConnectWithProfessional(int userId, int professionalId)
+        {
+            if (!userConnections.ContainsKey(userId))
+            {
+                userConnections[userId] = new HashSet<int>();
+            }
+
+            if (!userConnections.ContainsKey(professionalId))
+            {
+                userConnections[professionalId] = new HashSet<int>();
+            }
+
+            userConnections[userId].Add(professionalId);
+            userConnections[professionalId].Add(userId);
+        }
+
+        public void DisconnectWithProfessional(int userId, int professionalId)
+        {
+            if (userConnections.ContainsKey(userId))
+            {
+                userConnections[userId].Remove(professionalId);
+            }
+
+            if (userConnections.ContainsKey(professionalId))
+            {
+                userConnections[professionalId].Remove(userId);
+            }
+        }
+    }
+}
---
+++ FinNetworkTask/Program.cs
@@ -0,0 +1,15 @@
+﻿using FinNetworkTask;
+
+FintechNetwork network = new FintechNetwork();
+network.ShareInsight(1, 5);
+var insights = network.GetLatestInsights(1);
+Console.WriteLine(string.Join(", ", insights));
+
+network.ConnectWithProfessional(1, 2);
+network.ShareInsight(2, 6);
+insights = network.GetLatestInsights(1);
+Console.WriteLine(string.Join(", ", insights));
+
+network.DisconnectWithProfessional(1, 2);
+insights = network.GetLatestInsights(1);
+Console.WriteLine(string.Join(", ", insights));
